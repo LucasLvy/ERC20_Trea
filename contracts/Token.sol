fdff1709211152c8c014810c78cccdb0eaf84c69
LucasLvy <lucaslevy10@gmail.com>
2021-03-12 18:56:12 +0100

    Ajout d'un nombre maximal de tokens mintables et de la fonction getToken (cette fonction mint des tokens et les envoie a l'adresse qui a envoyé de l'ETH)

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,16 +4,18 @@ pragma solidity >=0.6.0 <0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-//private uint256 totalSupply=0;
+
 
 contract Trealos is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Trealos", "TRL") {
-        
+    uint256 private maxSupply;
+    constructor() ERC20("Trealos", "TRL") {
+        _setupDecimals(2);
+        maxSupply=1001002093092;
     }
 
-    function getToken() external payable {  
+    function getToken() external payable { 
+        require(totalSupply()+msg.value<maxSupply); 
         require(msg.value!=0);
         _mint(msg.sender, msg.value*100);
-        //totalSupply+=msg.value*100
     }
 }
