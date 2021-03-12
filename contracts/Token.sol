ff6f4dea96db5cea4371c83353a54cc150e3d80c
Quentin Gibon <quentingibon@wanadoo.fr>
2021-03-12 12:01:54 +0100

    quentin

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,11 +1,9 @@
 // contracts/GLDToken.sol
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity >=0.6.0 <0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-private uint256 etalonage3= 100;
-private uint256 etalonage2=10;
-private uint256 etalonage1=1;
+
 //private uint256 totalSupply=0;
 
 contract Trealos is ERC20 {
@@ -13,7 +11,7 @@ contract Trealos is ERC20 {
         
     }
 
-    getToken(){  
+    function getToken() external payable {  
         require(msg.value!=0);
         _mint(msg.sender, msg.value*100);
         //totalSupply+=msg.value*100
